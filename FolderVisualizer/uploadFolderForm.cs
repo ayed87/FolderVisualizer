cadc4d6dec57edcfbfbe06079945ac1f2c1f032e
ayed87 <gake12234@gmail.com>
2023-11-23 15:15:34 +0300

    Add project files.

---
+++ FolderVisualizer/uploadFolderForm.cs
@@ -0,0 +1,15 @@
+namespace FolderVisualizer
+{
+    public partial class uploadFolderForm : Form
+    {
+        public uploadFolderForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
